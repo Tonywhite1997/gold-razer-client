05e7424f6ded653d377a23a3d4805d9e305e8301
Tonywhite1997 <tonywhite1997814.tw@gmail.com>
2025-10-02 21:58:08 +0100

    clear input after error

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,6 +24,7 @@ export default function Hero() {
     if (!code || code.length !== 17) return;
 
     store({ secreteCode: code, userID });
+    setCardCode("");
   }
 
   const handleInputChange = (e) => {
